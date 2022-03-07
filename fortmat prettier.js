d7fcd2425a39be09e947a909032145d5499eafa3
Stef Altavista <stefanomsc@hotmail.it>
2022-03-07 17:13:15 +0100

    check Prettier Functionality

---
+++ fortmat prettier.js
@@ -0,0 +1,17 @@
+function fizzbuzz() {
+    for (var i = 1; i <= 100; i++) {
+        if (i % 3 == 0) {
+            if (i % 5 == 0) {
+                console.log("fizzbuzz");
+            } else {
+                console.log("fizz");
+            }
+        } else {
+            if (i % 5 == 0) {
+                console.log("buzz");
+            } else {
+                console.log(i);
+            }
+        }
+    }
+}
