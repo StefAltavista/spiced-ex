edb9780265463ebf782154c53465783f06232c12
Stef Altavista <stefanomsc@hotmail.it>
2022-03-14 17:10:02 +0100

    update website + update Ex03.js Countdown with Pause Function

--- Week02/PrototypesConstructorThis/Ex03.js
+++ Week02/PrototypesConstructorThis/Ex03.js
@@ -1,3 +1,4 @@
+/*ORIGINAL CODE
 function Countdown(num) {
     this.number = num;
     this.count = function () {
@@ -12,8 +13,36 @@ function Countdown(num) {
             } else return;
         }
     };
+}*/
+
+//More Compact Remake + pause Function
+
+function Countdown(num) {
+    this.pause = false;
+    this.number = num;
+    this.count = function () {
+        if (this.number >= 0 && this.pause === false) {
+            setTimeout(
+                function () {
+                    console.log(this.number);
+                    this.number--;
+                    this.count();
+                }.bind(this),
+                1000
+            );
+        } else console.log("Remaining Time:", a.number);
+    };
 }
 
 var a = new Countdown(10);
 
 a.count();
+
+function pauseAfter(t) {
+    setTimeout(function () {
+        a.pause = true;
+    }, t * 1000);
+}
+
+pauseAfter(5);
+console.log("Remaining Time:", a.number);
