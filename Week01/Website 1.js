8b23b59ae333ac578e370d8be72a4804592f8db7
Stef Altavista <stefanomsc@hotmail.it>
2022-03-17 18:06:27 +0100

    push Spiced Website with animated Menu

--- Week01/Website 1.js
+++ Week01/Website 1.js
@@ -1,6 +1,24 @@
 (function () {
-    var menu = document.getElementsByClassName("Hmenu");
-    console.log(menu);
+    var menuIcon = document.getElementsByClassName("menuIcon");
+    var wholeMenu = document.getElementsByClassName("hamburger-menu");
+    var closeButton = document.getElementsByTagName("span");
+    var closeClickOut = document.getElementById("darkLayer");
+    console.log(menuIcon);
+    console.log(wholeMenu[0].classList);
 
-    menu.addEventListener("click", function () {});
+    menuIcon[0].addEventListener("click", function () {
+        wholeMenu[0].classList.add("on");
+        wholeMenu[0].style.visibility = "visible";
+    });
+    closeButton[0].addEventListener("click", function (e) {
+        wholeMenu[0].classList.remove("on");
+        wholeMenu[0].style.visibility = "hidden";
+    });
+
+    wholeMenu[0].addEventListener("click", function (e) {
+        wholeMenu[0].classList.remove("on");
+        wholeMenu[0].style.visibility = "hidden";
+        console.log("here");
+        e.stopPropagation();
+    });
 })();
