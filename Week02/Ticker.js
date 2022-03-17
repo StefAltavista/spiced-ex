831ea098dc81532af22f0fa075ec1ef6f14e61fd
Stef Altavista <stefanomsc@hotmail.it>
2022-03-17 11:43:19 +0100

    pushing Ticker Part 2 completed

--- Week02/Ticker.js
+++ Week02/Ticker.js
@@ -1,7 +1,7 @@
 (function () {
     var albums = document.getElementById("albums");
     var links = albums.getElementsByTagName("A");
-
+    var animationID;
     console.log(albums);
 
     var left = albums.offsetLeft;
@@ -10,12 +10,26 @@
     function ticker() {
         left--;
         if (left <= -links[0].offsetWidth) {
-            left = left + links[0].offsetWidth;
+            left = 0;
             albums.appendChild(links[0]);
         }
 
         albums.style.left = left + "px";
-        requestAnimationFrame(ticker);
+        animationID = requestAnimationFrame(ticker);
     }
     ticker();
+
+    for (var i = 0; i < links.length; i++) {
+        console.log(links[i]);
+
+        links[i].addEventListener("mouseenter", function (e) {
+            console.log(e.target);
+            cancelAnimationFrame(animationID);
+        });
+
+        links[i].addEventListener("mouseleave", function (e) {
+            ticker();
+            console.log(e.target);
+        });
+    }
 })();
