a419b93723561202d86c78a0d517423be3ac0db5
Stef Altavista <stefanomsc@hotmail.it>
2022-03-21 17:56:00 +0100

    push Carousel Part 2, CssGrid Exercises

--- Week02/Carousel.js
+++ Week02/Carousel.js
@@ -1,27 +1,51 @@
 (function () {
     var carousel = document.getElementsByClassName("carousel");
     var cats = document.getElementsByTagName("img");
+    var dots = document.getElementsByClassName("dot");
 
     var i = 0;
-    scroll();
+    var transitioning = false;
+    var timeoutId;
+
+    timeoutId = setTimeout(scroll, 3000);
+
     carousel[0].addEventListener("transitionend", function (e) {
         if (e.target.classList.contains("offscreen")) {
             e.target.classList.remove("offscreen");
+            transitioning = false;
         }
     });
 
-    function scroll() {
-        setTimeout(function () {
-            cats[i].classList.remove("onscreen");
-            cats[i].classList.add("offscreen");
-            i++;
+    function scroll(idx) {
+        transitioning = true;
+        cats[i].classList.remove("onscreen");
+        cats[i].classList.add("offscreen");
+        dots[i].classList.remove("on");
 
-            if (i === cats.length) {
-                i = 0;
-            }
-            cats[i].classList.add("onscreen");
+        i++;
 
-            scroll();
-        }, 5000);
+        if (i === cats.length) {
+            i = 0;
+        }
+
+        if (idx != undefined) {
+            i = idx;
+        }
+
+        cats[i].classList.add("onscreen");
+        dots[i].classList.add("on");
+
+        timeoutId = setTimeout(scroll, 3000);
     }
+
+    Array.prototype.slice.call(dots).forEach(function (dot, index) {
+        dot.addEventListener("click", function () {
+            if (transitioning == true || index == i) {
+                return;
+            } else {
+                clearTimeout(timeoutId);
+                scroll(index);
+            }
+        });
+    });
 })();
