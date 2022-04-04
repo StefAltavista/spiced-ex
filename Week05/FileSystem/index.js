04034442250e94c13679ba4b53bd2d352acaaaf7
Stef Altavista <stefanomsc@hotmail.it>
2022-04-04 15:23:34 +0200

    Fun with Fs part 2

--- Week05/FileSystem/index.js
+++ Week05/FileSystem/index.js
@@ -1,4 +1,5 @@
 let fs = require("fs");
+const { isDate } = require("util/types");
 let chalk = require("/Users/Stef/Spiced Academy/aspartame-code/Week04/NodeAdventure/node_modules/chalk");
 let path = `${__dirname}/files`;
 
@@ -11,7 +12,7 @@ function logSizes(path) {
             let size = fs.statSync(path).size;
             console.log(
                 path,
-                chalk.red(item.name),
+                chalk.red.bold(item.name),
                 size,
                 chalk.yellow("bytes")
             );
@@ -20,3 +21,22 @@ function logSizes(path) {
         }
     });
 }
+
+let map = {};
+mapSizes(path, map);
+
+function mapSizes(path, obj) {
+    let items = fs.readdirSync(path, { withFileTypes: true });
+
+    items.forEach(function (item) {
+        if (item.isFile()) {
+            let size = fs.statSync(path).size;
+            obj[item.name] = size;
+        } else {
+            obj[item.name] = {};
+            mapSizes(`${path}/${item.name}`, obj[item.name]);
+        }
+    });
+}
+console.log(JSON.stringify(map, null, 4));
+fs.writeFileSync(`${__dirname}/map.json`, JSON.stringify(map, null, 4));
