f485046a4795a5f93f7495ba163b3c02251e1018
Stef Altavista <stefanomsc@hotmail.it>
2022-04-11 19:11:28 +0200

    update Express Portfolio server with partials

--- Week05/HandlebarsExp/index.js
+++ Week05/HandlebarsExp/index.js
@@ -7,13 +7,29 @@ const { engine } = require("express-handlebars");
 app.engine("handlebars", engine());
 app.set("view engine", "handlebars");
 //
+
+//get files from folders
 app.use(express.static("./public"));
 app.use(express.static("./projects"));
+
+//server set-up
+
 app.get("/", (req, res) => {
     res.render("home", {
         projects,
         title: "home",
         style: "style.css",
+    });
+});
+
+app.get("/projects/:name", (req, res) => {
+    const { name } = req.params;
+    const foundProj = projects.find((x) => x.directory === name);
+    res.render("project", {
+        projects,
+        foundProj,
+        title: foundProj.name,
+        style: "../projStyle.css",
     });
 });
 app.listen(3000, () => {
